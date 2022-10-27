ac953df3348f38ff4c4a23f1cb3ee157bb80e3f5
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2022-10-27 18:33:08 -0300

    remove /build folder in .gitignore and remove github/workflows

---
+++ build/precache-manifest.b68091468f95b97fd8517c171862dca7.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2b698966a5679c81828fa5df55b09c7",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/index.html"
+  },
+  {
+    "revision": "7c54863764e298e3bebb",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/css/main.7bf8c654.chunk.css"
+  },
+  {
+    "revision": "d3818d7240bc2d9bdeaf",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/js/2.20384472.chunk.js"
+  },
+  {
+    "revision": "1014b76b1932b9c4caafc220184395a7",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/js/2.20384472.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5e644e8670447bb426c",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/js/3.cb874919.chunk.js"
+  },
+  {
+    "revision": "7c54863764e298e3bebb",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/js/main.e5a8edcd.chunk.js"
+  },
+  {
+    "revision": "56e9d0eb6f9a80bf6b61",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/js/runtime-main.0c4b4fc3.js"
+  },
+  {
+    "revision": "94fbe5148d34be10271902cb4571f4cd",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/media/access-app.94fbe514.svg"
+  },
+  {
+    "revision": "c77d388b4fe1c1b5de19a4c28d6e24b6",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/media/arrow.c77d388b.svg"
+  },
+  {
+    "revision": "d3874976beb998b1f533799f95bcb7ad",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/media/background-business-network.d3874976.png"
+  },
+  {
+    "revision": "65e44e96258fa27f5afb4c02f27963dc",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/media/background-hot-seat.65e44e96.png"
+  },
+  {
+    "revision": "89255ee6962b5f4eed72554824c966be",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/media/background-page-1.89255ee6.png"
+  },
+  {
+    "revision": "0abc6ee0bcc62e964fa891d888b6b39a",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/media/background-page-4.0abc6ee0.png"
+  },
+  {
+    "revision": "b92bd9087f66b5ecf4e5df146f8b092f",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/media/background-sparks.b92bd908.png"
+  },
+  {
+    "revision": "4296d49dafb6b5559e35f63fcc49ce4a",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/media/background-special-suport.4296d49d.png"
+  },
+  {
+    "revision": "20cc33ea9de1591fd9fbd817e6d6fe15",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/media/breno-chair.20cc33ea.png"
+  },
+  {
+    "revision": "ce3af51dfc82b5023685a432e08df1d0",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/media/breno-frame.ce3af51d.png"
+  },
+  {
+    "revision": "470644737b6a2dae6b18e51c37e9787d",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/media/consultoria-financeira.47064473.png"
+  },
+  {
+    "revision": "4eb5539117f0b1a8b659c5332e4826fa",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/media/credit-cards-accept.4eb55391.svg"
+  },
+  {
+    "revision": "70f276e85b0476e8fe50d1826a57b0b4",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/media/desenvolvimento-web.70f276e8.png"
+  },
+  {
+    "revision": "517ac73bf1f2b6ad1ab6fe82fd7e5d01",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/media/direcao-de-fotografia.517ac73b.png"
+  },
+  {
+    "revision": "b56d122efedbff10f51867cade067f84",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/media/flame-icon.b56d122e.svg"
+  },
+  {
+    "revision": "f4397904a3369d2e72c52a2345809000",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/media/immediate-access.f4397904.svg"
+  },
+  {
+    "revision": "d65063a417f82cea24526d8d6d9c889a",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/media/logo.d65063a4.png"
+  },
+  {
+    "revision": "8db10d28b866eba59764f655e26bef6e",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/media/stars.8db10d28.svg"
+  },
+  {
+    "revision": "2a67d8b74c5a1a5b6913ad83bf95e3ac",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/media/study.2a67d8b7.svg"
+  },
+  {
+    "revision": "e017db2dfec85ad7432685d844a0492b",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/media/suport.e017db2d.svg"
+  },
+  {
+    "revision": "b59fb5417aa96aaf3e8aff1b7a32ed69",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/media/texture-card-comment.b59fb541.png"
+  },
+  {
+    "revision": "b037127e27803cbff827927c8af54cfc",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/media/verify-icon.b037127e.svg"
+  },
+  {
+    "revision": "0021f9679935d41fb8eef32e32dc9b7a",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/media/video-content.0021f967.svg"
+  },
+  {
+    "revision": "67b516fb1063b0e04cf395914e9e764d",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/media/video-lesson.67b516fb.svg"
+  },
+  {
+    "revision": "b6f2a7c442f63e5f57e20c6a7f69aa14",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/media/vip-support.b6f2a7c4.svg"
+  },
+  {
+    "revision": "9cd750df15a4341d9b2be91a56e24688",
+    "url": "https://viniciusalvesrocha.github.io/jovens-de-negocios/build/static/media/yellow-arrow.9cd750df.svg"
+  }
+]);
